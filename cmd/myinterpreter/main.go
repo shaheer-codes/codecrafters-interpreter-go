9e97ce851755b008ac47d767f9fdc840d93aa543
Zarel <135550782+zarel-blessed@users.noreply.github.com>
2024-12-16 13:50:53 +0530

    [any message]

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -5,6 +5,19 @@ import (
 	"os"
 )
 
+// const (
+// 	LEFT_PAREN  rune = '('
+// 	RIGHT_PAREN rune = ')'
+// 	LEFT_BRACE  rune = '{'
+// 	RIGHT_BRACE rune = '}'
+// 	COMMA       rune = ','
+// 	DOT         rune = '.'
+// 	MINUS       rune = '-'
+// 	PLUS        rune = '+'
+// 	SEMICOLON   rune = ';'
+// 	STAR        rune = '*'
+// )
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
@@ -21,18 +34,75 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Uncomment this block to pass the first stage
-
 	filename := os.Args[2]
-	fileContents, err := os.ReadFile(filename)
+	rawFileContents, err := os.ReadFile(filename)
+
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
 		os.Exit(1)
 	}
 
-	if len(fileContents) > 0 {
-		panic("Scanner not implemented")
-	} else {
-		fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
+	fileContents := string(rawFileContents)
+	contentsLength := len(fileContents)
+	exitCode := 0
+
+	for idx := 0; idx < contentsLength; idx++ {
+
+		switch fileContents[idx] {
+		case '(':
+			fmt.Println("LEFT_PAREN ( null")
+		case ')':
+			fmt.Println("RIGHT_PAREN ) null")
+		case '{':
+			fmt.Println("LEFT_BRACE { null")
+		case '}':
+			fmt.Println("RIGHT_BRACE } null")
+		case ',':
+			fmt.Println("COMMA , null")
+		case '.':
+			fmt.Println("DOT . null")
+		case '-':
+			fmt.Println("MINUS - null")
+		case '+':
+			fmt.Println("PLUS + null")
+		case ';':
+			fmt.Println("SEMICOLON ; null")
+		case '*':
+			fmt.Println("STAR * null")
+		case '=':
+			if idx+1 < contentsLength && fileContents[idx+1] == '=' {
+				fmt.Println("EQUAL_EQUAL == null")
+				idx++
+			} else {
+				fmt.Println("EQUAL = null")
+			}
+		case '!':
+			if idx+1 < contentsLength && fileContents[idx+1] == '=' {
+				fmt.Println("BANG_EQUAL != null")
+				idx++
+			} else {
+				fmt.Println("BANG ! null")
+			}
+		case '<':
+			if idx+1 < contentsLength && fileContents[idx+1] == '=' {
+				fmt.Println("LESS_EQUAL <= null")
+				idx++
+			} else {
+				fmt.Println("LESS < null")
+			}
+		case '>':
+			if idx+1 < contentsLength && fileContents[idx+1] == '=' {
+				fmt.Println("GREATER_EQUAL >= null")
+				idx++
+			} else {
+				fmt.Println("GREATER > null")
+			}
+		default:
+			fmt.Fprintf(os.Stderr, "[line 1] Error: Unexpected character: %v\n", string(fileContents[idx]))
+			exitCode = 65
+		}
 	}
+
+	fmt.Println("EOF  null")
+	os.Exit(exitCode)
 }

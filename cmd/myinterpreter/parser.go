4c9d50b4cfde382d802f874ac3c704ef5f12e260
zarel-blessed <zarelprogramming851815@gmail.com>
2024-12-19 21:02:25 +0530

    [any message]

--- cmd/myinterpreter/parser.go
+++ cmd/myinterpreter/parser.go
@@ -89,7 +89,10 @@ func (parser *Parser) parse_group() Group {
 		parser.advance()
 		var expr []string
 		for parser.peek().Lexeme != ")" && parser.peek().Kind != "EOF" {
-			expr = append(expr, string(parser.peek().Value))
+			if parser.peek().Lexeme == "(" {
+				expr = append(expr, parser.parse_group().Expr)
+			}
+			expr = append(expr, string(parser.parse_literal().Value))
 			parser.advance()
 		}
 		return Group{fmt.Sprintf("(group %v)", strings.Join(expr, " "))}
